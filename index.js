8e1462a4270e3e4e748aef30d01602c7e657cd8c
Michael Wuergler <wuergler@gmail.com>
2016-01-23 14:43:23 +0000

    use child process

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
 'use strict';
+var childProcess = require('child_process');
 var portfinder = require('portfinder');
 var chalk = require('chalk');
 var pify = require('pify');
-var serverInfo = {};
 
 require('native-promise-only');
 
-var server = require('http').createServer(function (req, res) {
-	res.end('noop-server excels at doing nothing.');
-});
-
-function killServer() {
-	try {
-		process.kill(serverInfo.pid, 'SIGKILL');
-	} catch (err) {
-		console.log('cant killServer with err:', err);
-	}
-}
-
-process.on('exit', function () {
-	killServer();
-});
+var serverInfo = {};
 
 module.exports = function (opts) {
 	opts = opts || {};
 
 	return new Promise(function (resolve, reject) {
 		pify(portfinder.getPort)().then(function (port) {
-			server.listen(port, function (err) {
-				if (err) {
-					reject(err);
-				}
-			});
-
 			serverInfo.port = port;
 			serverInfo.pid = process.pid;
 
-			console.log(chalk.red('noop-server'), chalk.cyan('port:'), chalk.cyan(serverInfo.port));
-			console.log(chalk.red('noop-server'), chalk.yellow('pid:'), chalk.yellow(serverInfo.pid));
+			var cp = childProcess.spawn('node', ['./server.js', '--port=' + port]);
 
-			resolve(serverInfo);
+			cp.stdout.on('error', reject);
+
+			cp.stdout.setEncoding('utf8');
+
+			cp.stdout.on('data', function (data) {
+				if (data.trim() === 'ok') {
+					log();
+					cp.stdio = ['ignore', 'ignore', 'ignore'];
+					resolve(serverInfo);
+				}
+			});
+			cp.stderr.on('data', function (data) {
+				console.log('cp.stderr: ', data);
+			});
+			cp.unref();
 		}).catch(function (err) {
 			console.log('noop-server not launched.');
-			console.log('err:', err);
+			reject(err);
 		});
 	});
 };
+
+function log(serverInfo) {
+	console.log(chalk.red('noop-server'), chalk.cyan('port:'), chalk.cyan(serverInfo.port));
+	console.log(chalk.red('noop-server'), chalk.yellow('pid:'), chalk.yellow(serverInfo.pid));
+}
 
